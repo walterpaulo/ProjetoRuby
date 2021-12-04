a00163b3b67268923a83f8945170b3d2f6eaf5e6
walterpaulo <walter0paulo@hotmail.com>
2021-12-04 15:32:01 -0300

    add funçao

---
+++ README.md
@@ -0,0 +1,1 @@
+# ProjetoRuby
--- ValidarCPF.rb
+++ ValidarCPF.rb
@@ -5,6 +5,10 @@ def validarCPF(cpf)
 end
 
 cpf = 12345678910
+cpf2 = 53627340086
+
 result = validarCPF(cpf)
+result2 = validarCPF(cpf2)
 
 puts "O CPF, #{cpf} é válido: #{result}"
+puts "O CPF, #{cpf2} é válido: #{result2}"
